5c4c1aa6f51906776cd8d02b29e7298b9bfc39aa
lcbiplove <lcbiplove1@gmail.com>
2020-05-18 17:35:09 +0545

    Style to home page

--- index_tmp.php
+++ index_tmp.php
@@ -1,28 +1,33 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="index.css"/>
-</head>
-<body>
-<a href="indes_tmp.php"><h3 style="float:left;">Home</h3></a>
-<div class="main-top">
-<h2 style="color:red;">yo kura kasailai na vannu ni feri!!!</h2>
-<div style="display:flex;" class="top">
-<form method="GET" action="search_tmp.php" >
-    <input class="search-box" type="text" name="search" placeholder="search yourself"></input>
-    <input class="search" type="submit" placeholder="search" value="search"></input>
-</form>
-<?php 
-    echo '<a href="visit_tmp.php?who=">
-                <button class="btn">random</button></a>
-        </div>'; 
-?>
-<div class="foot">
-<p> by using this site you agreed this data doesnt affect anything
-</p>
-</div>
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="index.css"/>
+    </head>
+    <body>
+        <div class="home-nav">
+            <div>
+                <div class="home-div">
+                    <a href="index_tmp.php">Home</a>
+                </div>
+                <div class="home-search-items">
+                    <form method="GET" action="search_tmp.php" >
+                        <input class="search-box" type="search" name="search" placeholder="Search yourself..." autofocus="on" />
+                        <button type="submit">Search</button>
+                        <?php 
+                            echo '<a href="visit_tmp.php?who="><button type="button" class="btn">Random</button></a>'; 
+                        ?>
+                    </form>
+                </div>
+            </div>
+        </div>
+        <div class="foot">
+            <div> 
+                By using this site you agreed this data doesnt affect anything
+            </div>
+            <span>&times;</span>
+        </div> 
+    </body>
 </html>
