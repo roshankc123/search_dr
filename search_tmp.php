78e197800f73314369bdf272d240dda6e299741f
lcbiplove <lcbiplove1@gmail.com>
2020-05-19 15:10:04 +0545

    Search nav for all addedd

--- search_tmp.php
+++ search_tmp.php
@@ -1,20 +1,36 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="index.css"/>
-</head>
-<body>
-<a href="/"><h3 style="float:left;">Home</h3></a>
-<div class="main-top">
-<h2 style="color:red;">yo kura kasailai na vannu ni feri!!!</h2>
-<div style="display:flex;" class="top">
-<form method="GET" action="index.php" >
-    <input class="search-box" type="text" name="search" placeholder="search yourself"></input>
-    <input class="search" type="submit" placeholder="search" value="search"></input>
-</form>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="index.css"/>
+    </head>
+    <body>
+        <div class="main-search-nav">
+            <div class="home-div">
+                <a href="index_tmp.php">Home</a>
+            </div>
+            <div class="home-search-items">
+                <form method="GET" action="search_tmp.php" >
+                    <input class="search-box" type="search" name="search" placeholder="Search yourself..."/>
+                    <button type="submit">Search</button>
+                    <a href="visit_tmp.php?who="><button type="button" class="btn">Random</button></a>
+                </form>
+            </div>
+        </div>
+        <div class="foot">
+            <div> 
+                By using this site you agreed this data doesnt affect anything
+            </div>
+            <span>&times;</span>
+        </div> 
+    </body>
+</html>
+
+
+<!--
+    
 <?php 
     echo '<a href="visit_tmp.php?who=">
                 <button class="btn">random</button></a>
@@ -34,9 +50,4 @@
     }
     echo "</div>";
 ?>
-<div class="foot">
-<p> by using this site you agreed this data doesnt affect anything
-</p>
-</div>
-</body>
-</html>
+->
