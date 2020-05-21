f94c4ec67d5806e3fdf44c761dc62ea7f1ab95ba
lcbiplove <lcbiplove1@gmail.com>
2020-05-21 16:30:05 +0545

    Js updated

--- api/local/main.php
+++ api/local/main.php
@@ -2,7 +2,7 @@
 include '../include.php';
 $data=new main_get_data;
     if($_GET['search']){
-        echo $data->search_result($_GET['who']);
+        echo $data->search_result($_GET['search']);
     }
     else if($_GET['who']){
         echo $data->visit($_GET['who']);
